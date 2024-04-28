50ff52855475b6290f74594436ae8eb07b9958ef
Xuan Linh <140143092+NguyenDangXuanLinh@users.noreply.github.com>
2024-04-29 00:50:14 +0700

    Update and rename Intermediate - Window Functions.sql to Window Functions.sql

--- Window Functions.sql
+++ Window Functions.sql
@@ -1,13 +1,21 @@
--- Window Functions
+-- Window Functions: OVER()
+# OVER(PATITION BY __) | OVER(PATITION BY __ GROUP BY__)  : more funtionality 
+-- Allow us to look at a partition or a group, but they each keep their own unique rows in the output
 
--- windows functions are really powerful and are somewhat like a group by - except they don't roll everything up into 1 row when grouping. 
--- windows functions allow us to look at a partition or a group, but they each keep their own unique rows in the output
--- we will also look at things like Row Numbers, rank, and dense rank
+# ROW_NUMBER()  |  RANK()  | DENSE_RANK() : often used with WF	
+-- Summary of the differences in those functions:
+	-- row_number + rank: both return unique serial numbers
+		-- rank: repeats the unique number if values are similar
+		-- row_number: continue assign unique numbers despite the similarity.
+	-- only dense_rank: return the values after ranking it in the PARTITION BY clause
+	
 
 SELECT * 
 FROM employee_demographics;
 
+#Let compare Group By and WF
 -- first let's look at group by
+	
 SELECT gender, ROUND(AVG(salary),1)
 FROM employee_demographics dem
 JOIN employee_salary sal
@@ -15,20 +23,21 @@ JOIN employee_salary sal
 GROUP BY gender
 ;
 
--- now let's try doing something similar with a window function
+-- now doing something similar with a window function (WF)
 
 SELECT dem.employee_id, dem.first_name, gender, salary,
-AVG(salary) OVER()
+	AVG(salary) OVER()
 FROM employee_demographics dem
 JOIN employee_salary sal
 	ON dem.employee_id = sal.employee_id
 ;
 
--- now we can add any columns and it works. We could get this exact same output with a subquery in the select statement, 
--- but window functions have a lot more functionality, let's take a look
+-- We can use AVG without doing a subquery in the select statement and it works 
+# Doing WF keeps everything cleaner and shorter than subquery.
 
-
--- if we use partition it's kind of like the group by except it doesn't roll up - it just partitions or breaks based on a column when doing the calculation
+-- When add PATITION ON WF: it is just partitions or breaks based on a column when doing the calculation
+-- Similar to Group By except it doesn't roll up
+	
 
 SELECT dem.employee_id, dem.first_name, gender, salary,
 AVG(salary) OVER(PARTITION BY gender)
@@ -38,7 +47,7 @@ JOIN employee_salary sal
 ;
 
 
--- now if we wanted to see what the salaries were for genders we could do that by using sum, but also we could use order by to get a rolling total
+-- Let's see the salaries were for genders with a rolling total (cumulative sume)
 
 SELECT dem.employee_id, dem.first_name, gender, salary,
 SUM(salary) OVER(PARTITION BY gender ORDER BY employee_id)
@@ -48,9 +57,12 @@ JOIN employee_salary sal
 ;
 
 
--- Let's look at row_number rank and dense rank now
+-- Let's look at row_number, rank and dense rank now
 
+#ROW_NUMBER(): give unique numbers for a partition, and the order of those numbers are based on 
+	ORDER BY clause
 
+-- To see the order of highest paid employees by gender	
 SELECT dem.employee_id, dem.first_name, gender, salary,
 ROW_NUMBER() OVER(PARTITION BY gender)
 FROM employee_demographics dem
@@ -58,7 +70,7 @@ JOIN employee_salary sal
 	ON dem.employee_id = sal.employee_id
 ;
 
--- let's  try ordering by salary so we can see the order of highest paid employees by gender
+-- To see the order of highest paid employees by gender, but ordering it by salary 
 SELECT dem.employee_id, dem.first_name, gender, salary,
 ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary desc)
 FROM employee_demographics dem
@@ -66,7 +78,11 @@ JOIN employee_salary sal
 	ON dem.employee_id = sal.employee_id
 ;
 
--- let's compare this to rank
+
+#RANK(): also give serial unique numbers, but similar value assigns the same number 
+	then the next unique number = previous number - how many similar values before
+	
+-- let's compare row_number() and rank()
 SELECT dem.employee_id, dem.first_name, gender, salary,
 ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary desc) row_num,
 Rank() OVER(PARTITION BY gender ORDER BY salary desc) rank_1 
@@ -75,9 +91,12 @@ JOIN employee_salary sal
 	ON dem.employee_id = sal.employee_id
 ;
 
--- notice rank repeats on tom ad jerry at 5, but then skips 6 to go to 7 -- this goes based off positional rank
+-- notice rank repeats on tom and jerry at 5, 
+-- but then skips 6, and continue with 7 -- this goes based off positional rank
 
 
+#DENSE_RANK() : affected by ORDER BY clause so it returns the values after ranking it 
+	
 -- let's compare this to dense rank
 SELECT dem.employee_id, dem.first_name, gender, salary,
 ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary desc) row_num,
@@ -88,7 +107,8 @@ JOIN employee_salary sal
 	ON dem.employee_id = sal.employee_id
 ;
 
-
+-- notice that RANK returns numbers affected by PARTITION BY clause
+-- that the different between RANK and DENSE_RANK. 
 
 
 
