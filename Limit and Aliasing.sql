6e31fa97a7ba4146b48f2c3225e74c53ac3491fe
Xuan Linh <140143092+NguyenDangXuanLinh@users.noreply.github.com>
2024-04-28 17:21:52 +0700

    Update and rename Beginner - Limit and Aliasing.sql to Limit and Aliasing.sql

--- Limit and Aliasing.sql
+++ Limit and Aliasing.sql
@@ -1,7 +1,6 @@
 -- LIMIT and ALIASING
 
--- Limit is just going to specify how many rows you want in the output
-
+-- LIMIT is just going to specify how many rows you want in the output
 
 SELECT *
 FROM employee_demographics
@@ -14,7 +13,7 @@ FROM employee_demographics
 ORDER BY first_name
 LIMIT 3;
 
--- now there is an additional paramater in limit which we can access using a comma that specifies the starting place
+-- There is an additional paramater in limit which we can access using a comma that specifies the starting place
 
 SELECT *
 FROM employee_demographics
@@ -26,9 +25,8 @@ ORDER BY first_name
 LIMIT 3,2;
 
 -- this now says start at position 3 and take 2 rows after that
--- this is not used a lot in my opinion
 
--- you could us it if you wanted to select the third oldest person by doing this:
+-- select the third oldest person by doing this:
 SELECT *
 FROM employee_demographics
 ORDER BY age desc;
@@ -42,8 +40,7 @@ LIMIT 2,1;
 -- ALIASING
 
 -- aliasing is just a way to change the name of the column (for the most part)
--- it can also be used in joins, but we will look at that in the intermediate series
-
+-- it can also be used in joins
 
 SELECT gender, AVG(age)
 FROM employee_demographics
@@ -54,14 +51,6 @@ SELECT gender, AVG(age) AS Avg_age
 FROM employee_demographics
 GROUP BY gender
 ;
-
--- although we don't actually need it, but it's more explicit which I usually like
-SELECT gender, AVG(age) Avg_age
-FROM employee_demographics
-GROUP BY gender
-;
-
-
 
 
 
